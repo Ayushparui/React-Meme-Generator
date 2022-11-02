815a9f326a1586e0f47ab3b388229719762348d5
Ayushparui <paruiayush900@gmail.com>
2022-11-02 10:31:09 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+ function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={require("../images/troll-face.png")} 
+                className="header--image"
+                alt="troll-face"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
+
+export default Header
